cf643055d39bcf46fc6deb9cde06eff78574b9e1
Alex Rivera <alexriveracruz4@gmail.com>
2021-10-22 06:33:08 +0000

    [UPDATE] quit connect import

--- frontend_jbh/src/Estudiantes/Componentes/LoginEstudiantes/Login/Login.js
+++ frontend_jbh/src/Estudiantes/Componentes/LoginEstudiantes/Login/Login.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Login.css';
 //import { useHistory } from 'react-router-dom';
 import { withRouter } from "react-router-dom"
-import { connect } from 'react-redux';
+
 
 /*
 function Login() {
