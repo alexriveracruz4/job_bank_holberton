2aa4203c0adf48114ad9ff28c0fe4f646b709729
KennyReyesS <kennyreyes2000@gmail.com>
2022-01-25 15:05:01 -0500

    text removed

--- jobbank-app/src/Home/Componentes/Main/Main.js
+++ jobbank-app/src/Home/Componentes/Main/Main.js
@@ -5,9 +5,6 @@ import mysvg from "../images/Magnifying_glass_icon.svg"
 function Main() {
   return (
     <main className="padding-main">
-      <div className="text-main">
-        <h1>Holberton en el Mundo</h1>
-      </div>
       <div className="container padding">
         <div class="row">
           <nav class="navbar navbar-expand-lg navbar-dark">
@@ -31,21 +28,10 @@ function Main() {
               </ul>
             </div>
           </nav>
-          {/*<div className="Main-content">
-            <h1>Hello</h1>
-          </div>*/}
         </div>
         <div className="title-buttons">
           Preguntas frecuentes sobre contratar un desarrollador
         </div>
-        {/*<div class="btn-group-vertical" role="group" aria-label="Vertical button group">
-          <button type="button" class="btn btn-light">¿Cómo contratar el mejor programador?</button>
-          <button type="button" class="btn btn-light">¿Dónde puedo contratar programadores?</button>
-          <button type="button" class="btn btn-light">¿Cómo contactar a un programador?</button>
-          <button type="button" class="btn btn-light">¿Cómo encontrar desarrolladores?</button>
-          <button type="button" class="btn btn-light">¿Dónde encontrar programadores freelance?</button>
-          <button type="button" class="btn btn-light">¿Qué es Holberton?</button>
-        </div>*/}
         <div className="Accordion-FAQ">
           <Accordion>
             <Accordion.Item eventKey="0">
