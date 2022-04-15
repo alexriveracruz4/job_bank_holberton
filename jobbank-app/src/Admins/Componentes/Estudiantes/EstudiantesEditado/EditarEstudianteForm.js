6518e4dad7af0c85aa9dc64c5830113b39dfe978
KennyReyesS <kennyreyes2000@gmail.com>
2022-04-14 20:18:07 -0500

    iframe video in form added

--- jobbank-app/src/Admins/Componentes/Estudiantes/EstudiantesEditado/EditarEstudianteForm.js
+++ jobbank-app/src/Admins/Componentes/Estudiantes/EstudiantesEditado/EditarEstudianteForm.js
@@ -91,7 +91,33 @@ const CrudForm = ({ updateData, dataToEdit }) => {
     setStudent(applications);
   }
 
-    // Get photoname
+  // Get video id
+  function getVideo_id(url) {
+    if (url !== null) {
+      var regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
+      if (regexp.test(url)) {
+        const a = url.match(/https:\/\/(:?www.)?(\w*)/)[2];
+        if (a === "youtube") {
+          var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+          var match = url.match(regExp);
+          const video_id = (match&&match[7].length==11)? match[7] : false;
+          return video_id
+        } else if (a === "vimeo") {
+        var regExp = /^.*(vimeo\.com\/)((channels\/[A-z]+\/)|(groups\/[A-z]+\/videos\/))?([0-9]+)/;
+        var parseUrl = regExp.exec(url)
+        return parseUrl[5]
+        } else {
+          return null
+        }
+      } else {
+        return null
+      }
+    } else {
+      return null
+    }
+  }
+
+  // Get photoname
 
   useEffect(() => {
   let photoname = "";
@@ -1024,6 +1050,16 @@ const CrudForm = ({ updateData, dataToEdit }) => {
               <i className="fas fa-exclamation-circle" />
             </div>
             <small id='smallVideoLink'> Error message </small>
+            {form.video_link
+                ? <div className='display-video' style={{display: 'flex', justifyContent: 'center'}}>
+                    <iframe src={`https://www.youtube.com/embed/${getVideo_id(form.video_link)}`}
+                      frameborder='0'
+                      allow='autoplay; encrypted-media'
+                      allowfullscreen
+                      title='video'
+                      style={{width: '80%', height: '40vh'}}
+                    /></div>
+                : null}
           </div>
 
 
